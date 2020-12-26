9ca6dc37e0de5847acb53a38c1d318fc46c8227f
roopekj <40203658+roopekj@users.noreply.github.com>
2020-12-26 18:33:48 +0200

    Add board and initial structure

---
+++ js/script.js
@@ -0,0 +1,54 @@
+var board = null
+var game = new Chess()
+
+function onDragStart (source, piece, position, orientation) {
+  if (game.game_over()) return false
+
+  if (piece.search(/^b/) !== -1) return false
+}
+
+function makeRandomMove () {
+  var possibleMoves = game.moves()
+
+  if (possibleMoves.length === 0) return
+
+  var randomIdx = Math.floor(Math.random() * possibleMoves.length)
+  game.move(possibleMoves[randomIdx])
+  board.position(game.fen())
+}
+
+function resetBoard() {
+  game = new Chess()
+  board.position(game.fen())
+}
+
+function makeMove() {
+
+}
+
+function onDrop (source, target) {
+  var move = game.move({
+    from: source,
+    to: target,
+    promotion: 'q'
+  })
+
+  if (move === null) return 'snapback'
+
+  
+
+  window.setTimeout(makeRandomMove, 250)
+}
+
+function onSnapEnd () {
+  board.position(game.fen())
+}
+
+var config = {
+  draggable: true,
+  position: 'start',
+  onDragStart: onDragStart,
+  onDrop: onDrop,
+  onSnapEnd: onSnapEnd
+}
+board = Chessboard('myBoard', config)
